8fb7b4e12f008636d3d50e752bb7c54fff9d105c
eunseo <songes04@gmail.com>
2025-03-24 08:43:31 +0900

    [#3] refactor: separate router logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Landing from './pages/Landing';
-import Login from './pages/auth/Login';
-import Signup from './pages/auth/Signup';
-import SignupStep1 from './pages/auth/SignupStep1';
-import SignupStep2 from './pages/auth/SignupStep2';
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './routes/Router';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        
-        {/* 회원가입 중첩 라우팅 */}
-        <Route path="/signup" element={<Signup />}>
-          <Route index element={<SignupStep1 />} />
-          <Route path="step1" element={<SignupStep1 />} />
-          <Route path="step2" element={<SignupStep2 />} />
-        </Route>
-      </Routes>
-    </Router>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
