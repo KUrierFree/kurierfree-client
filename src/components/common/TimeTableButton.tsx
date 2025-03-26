24f1fec019256c148cd828721a940d400926c62a
eunseo <songes04@gmail.com>
2025-03-27 01:18:42 +0900

    [#8] feat: add table meta type and separate TimeTableButton component

---
+++ src/components/common/TimeTableButton.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+interface TimeTableButtonProps {
+  onClick: () => void;
+}
+
+const TimeTableButton = ({ onClick }: TimeTableButtonProps) => (
+  <button
+    className="p-1 text-gray-500 hover:text-gray-700"
+    onClick={onClick}
+  >
+    <svg
+      className="w-5 h-5"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1.5}
+        d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+      />
+    </svg>
+  </button>
+);
+
+export default TimeTableButton; 
---
+++ src/constants/tables.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { ColumnDef, TableMeta } from "@tanstack/react-table";
+import { DisabledStudent, Supporter } from "../types/user";
+import MatchingStatusBadge from "../components/common/MatchingStatusBadge";
+import TimeTableButton from "../components/common/TimeTableButton";
+
+interface TableMetaWithTimeTable<T> extends TableMeta<T> {
+  onTimeTableClick: () => void;
+}
+
+// 장애학생 테이블 컬럼 정의
+export const DISABLED_STUDENT_COLUMNS: ColumnDef<DisabledStudent, any>[] = [
+  {
+    header: "이름",
+    accessorKey: "name",
+  },
+  {
+    header: "학과",
+    accessorKey: "department",
+  },
+  {
+    header: "성별",
+    accessorKey: "gender",
+  },
+  {
+    header: "장애유형",
+    accessorKey: "disabilityType",
+  },
+  {
+    header: "매칭",
+    accessorKey: "matchingStatus",
+    cell: ({ getValue }) => (
+      <MatchingStatusBadge status={getValue()} />
+    ),
+  },
+  {
+    header: "시간표",
+    cell: ({ table }) => (
+      <TimeTableButton onClick={() => (table.options.meta as TableMetaWithTimeTable<DisabledStudent>).onTimeTableClick()} />
+    ),
+  },
+];
+
+// 서포터즈 테이블 컬럼 정의
+export const SUPPORTERS_COLUMNS: ColumnDef<Supporter, any>[] = [
+  {
+    header: "이름",
+    accessorKey: "name",
+  },
+  {
+    header: "학과",
+    accessorKey: "department",
+  },
+  {
+    header: "성별",
+    accessorKey: "gender",
+  },
+  {
+    header: "학년",
+    accessorKey: "grade",
+  },
+  {
+    header: "매칭",
+    accessorKey: "matchingStatus",
+    cell: ({ getValue }) => (
+      <MatchingStatusBadge status={getValue()} />
+    ),
+  },
+  {
+    header: "시간표",
+    cell: ({ table }) => (
+      <TimeTableButton onClick={() => (table.options.meta as TableMetaWithTimeTable<Supporter>).onTimeTableClick()} />
+    ),
+  },
+]; 
