952cf6737122d348f03d37e7b5266162859b0a47
eunseo <songes04@gmail.com>
2025-04-05 01:41:38 +0900

    [#7][#8] refactor: create SupporterTab and SupporterTable

--- src/pages/admin/matching/tabs/SupporterTab.tsx
+++ src/pages/admin/matching/tabs/SupporterTab.tsx
@@ -1,13 +1,35 @@
 import React from "react";
-import SupporterStatusTable from "../../../../components/admin/matching/Supporter/SupporterStatusTable";
 import { Supporter } from "../../../../types/user";
+import SupportersTable from "../../../../components/admin/matching/supporters/SupportersTable";
 
-interface SupporterTabProps {
-  supporters: Supporter[];
-}
+// 더미 데이터
+const dummySupporters: Supporter[] = [
+  {
+    id: 1,
+    name: "서포터1",
+    gender: "남성",
+    department: "컴퓨터공학과",
+    grade: "2학년",
+    matchingStatus: "waiting",
+    supportType: "수업도우미"
+  },
+  {
+    id: 2,
+    name: "서포터2",
+    gender: "여성",
+    department: "전자공학과",
+    grade: "3학년",
+    matchingStatus: "waiting",
+    supportType: "수업도우미"
+  }
+];
 
-const SupporterTab: React.FC<SupporterTabProps> = ({ supporters }) => {
-  return <SupporterStatusTable data={supporters} />;
+const SupporterTab: React.FC = () => {
+  return (
+    <SupportersTable
+      data={dummySupporters}
+    />
+  );
 };
 
 export default SupporterTab;
