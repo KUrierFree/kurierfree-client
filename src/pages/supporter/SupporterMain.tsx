43e1db2590140ed52a9cd15cc57683ebb2498f53
eunseo <songes04@gmail.com>
2025-05-28 21:05:47 +0900

    [#16] feat: Header와 Footer import (로그인 상태의 header 컴포넌트 개발 필요)

--- src/pages/supporter/SupporterMain.tsx
+++ src/pages/supporter/SupporterMain.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from '../../components/layout/Header';
+import Footer from '../../components/layout/Footer';
 
 const SupporterMain: React.FC = () => {
   return (
     <div className="min-h-screen bg-white">
-      <h1>SupporterMain</h1>
+      <Header />
+      <div className="max-w-[1200px] mx-auto px-4 mt-16">
+        <h1>SupporterMain</h1>
+      </div>
+      <Footer />
     </div>
   );
 };
